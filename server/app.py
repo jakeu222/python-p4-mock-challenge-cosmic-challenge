0a9110ec7dac3b5214b7b1b02e472ef205c3f40b
Jake Underwood <jakeunderwood2211@yahoo.com>
2023-10-13 07:00:21 -0600

    complete

--- server/app.py
+++ server/app.py
@@ -19,12 +19,191 @@ app.json.compact = False
 migrate = Migrate(app, db)
 
 db.init_app(app)
+api = Api(app)
 
 
-@app.route('/')
-def home():
-    return ''
+@app.get('/scientists')
+def get_scientists():
+    response_dict = [s.to_dict(only=('id', 'name', 'field_of_study'))
+                     for s in Scientist.query.all()]
+    return make_response(response_dict, 200)
+
+
+@app.get('/scientists/<int:id>')
+def get_scientists_by_id(id):
+    sci = Scientist.query.filter_by(id=id).first()
+
+    if not sci:
+        return make_response({"error": "scientist not found"}, 404)
+
+    return make_response(sci.to_dict(), 200)
+
+
+@app.post('/scientists')
+def post_scientists():
+    data = request.get_json()
+    try:
+        new_scientist = Scientist(
+            name=data['name'],
+            field_of_study=data['field_of_study']
+        )
+    except Exception:
+        return make_response({"errors": ["validation errors"]}, 422)
+    db.session.add(new_scientist)
+    db.session.commit()
+    return make_response(new_scientist.to_dict(only=("id", 'name', 'field_of_study')), 201)
+
+
+@app.patch('/scientists/<int:id>')
+def patch_scientists(id):
+    sci = Scientist.query.filter_by(id=id).first()
+    data = request.get_json()
+
+    if not sci:
+        return make_response({"error": "Scientist not found"}, 404)
+
+    try:
+        for field in data:
+            setattr(sci, field, data[field])
+    except:
+        return make_response({"errors": ["validation errors"]}, 422)
+
+    db.session.commit()
+    return make_response(sci.to_dict(rules=('-mission_s_relationship',)), 202)
+
+
+@app.delete('/scientists/<int:id>')
+def delete_scientists(id):
+    sci = Scientist.query.filter_by(id=id).first()
+
+    if not sci:
+        return make_response({"error": "Scientist not found"}, 404)
+
+    db.session.delete(sci)
+    db.session.commit()
+
+    return make_response({"message": "This rizz was deleted by tri, you punk"}, 226)
+
+
+@app.get('/planets')
+def get_planets():
+    response_dict_list = [p.to_dict() for p in Planet.query.all()]
+    return make_response(response_dict_list, 200)
+
+
+@app.get('/missions')
+def get_missions():
+    response_dict_list = [m.to_dict() for m in Mission.query.all()]
+    return make_response(response_dict_list, 200)
+
+
+@app.post('/missions')
+def post_mission():
+    request_object = request.get_json()
+    try:
+        new_mission = Mission(
+            name=request_object['name'],
+            scientist_id=request_object['scientist_id'],
+            planet_id=request_object['planet_id']
+        )
+        db.session.add(new_mission)
+        db.session.commit()
+    except Exception as e:
+        # different way to show our errors
+        message = {'errors': [e.__str__()]}
+        return make_response(message, 422)
+    return make_response(new_mission.to_dict(), 201)
 
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
+# #!/usr/bin/env python3
+
+# from models import db, Scientist, Mission, Planet
+# from flask_restful import Api, Resource
+# from flask_migrate import Migrate
+# from flask import Flask, make_response, jsonify, request
+# import os
+
+# BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+# DATABASE = os.environ.get(
+#     "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
+
+
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.json.compact = False
+
+# migrate = Migrate(app, db)
+
+# db.init_app(app)
+
+
+# @app.route('/')
+# def home():
+#     return ''
+
+
+# @app.get('/scientists')
+# def get_all_scientists():
+#     scientists = Scientist.query.all()
+#     data = [scientist.to_dict(rules=('-mission',)) for scientist in scientists]
+
+#     return make_response(
+#         jsonify(data),
+#         200
+#     )
+
+
+# @app.get('/scientists/<int:id>')
+# def set_scientists(id):
+#     scientist = Scientist.query.filter(Scientist.id == id).first()
+
+#     if not scientist:
+#         return make_response(
+#             jsonify({"error": "Scientist not found"}),
+#             404
+#         )
+#     return make_response(
+#         jsonify(scientist.to_dict()),
+#         200
+#     )
+
+
+# @app.post('/scientists')
+# def post_scientists():
+#     data = request.get_json()
+#     print(data)
+
+#     try:
+#         new_scientist = Scientist(
+#             name=data['name'],
+#             field_of_study=data['field_of_study']
+#         )
+
+#     except Exception:
+#         return make_response({'errors': ['validation error']}, 422)
+#     db.sessions.add(new_scientist)
+#     db.session.commit()
+#     return make_response(new_scientist.to_dict(), 201)
+
+
+# # @app.patch('/scientist/<int:id>')
+# # def patch_scientist(id):
+# #     sci = Scientist.query.filter_by(id=id).first()
+# #     data=request.get_json()
+
+# #     if not sci:
+# #         return make_response({"error": "BJBHUVUY"},404)
+
+# #     try:
+# #         for field in data:
+# #             setattr(sci, field, data[field])
+# #     except:
+# #         return make_response({"error": ["validation error"]
+
+
+# if __name__ == '__main__':
+#     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,6 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-from sqlalchemy.orm import validates
+from sqlalchemy.orm import validates, relationship
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
@@ -17,44 +17,111 @@ metadata = MetaData(naming_convention=convention)
 db = SQLAlchemy(metadata=metadata)
 
 
-class Planet(db.Model, SerializerMixin):
-    __tablename__ = 'planets'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    distance_from_earth = db.Column(db.Integer)
-    nearest_star = db.Column(db.String)
-
-    # Add relationship
-
-    # Add serialization rules
-
-
 class Scientist(db.Model, SerializerMixin):
-    __tablename__ = 'scientists'
+    __tablename__ = 'scientist_table'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    field_of_study = db.Column(db.String, nullable=False)
+
+    mission_s_relationship = db.relationship(
+        'Mission', back_populates='scientist_field', cascade="all, delete")
+
+
+class Planet(db.Model, SerializerMixin):
+    __tablename__ = 'planet_table'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    field_of_study = db.Column(db.String)
+    nearest_star = db.Column(db.String)
+    distance_from_earth = db.Column(db.Integer)
 
-    # Add relationship
-
-    # Add serialization rules
-
-    # Add validation
+    mission_p_relationship = db.relationship(
+        'Mission', back_populates='planet_field', cascade="all, delete")
 
 
 class Mission(db.Model, SerializerMixin):
-    __tablename__ = 'missions'
+    __tablename__ = 'mission_table'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String, nullable=False)
+    scientist_id = db.Column(db.Integer, db.ForeignKey(
+        'scientist_table.id'), nullable=False)
+    scientist_field = db.relationship(
+        'Scientist', back_populates='mission_s_relationship')
 
-    # Add relationships
+    planet_id = db.Column(db.Integer, db.ForeignKey(
+        'planet_table.id'), nullable=False)
+    planet_field = db.relationship(
+        'Planet', back_populates='mission_p_relationship')
 
-    # Add serialization rules
+    serialize_rules = ('-scientist_field',
+                       '-planet_field.mission_p_relationship')
 
-    # Add validation
+
+# from flask_sqlalchemy import SQLAlchemy
+# from sqlalchemy import MetaData
+# from sqlalchemy.orm import validates
+# from sqlalchemy.ext.associationproxy import association_proxy
+# from sqlalchemy_serializer import SerializerMixin
+
+# convention = {
+#     "ix": "ix_%(column_0_label)s",
+#     "uq": "uq_%(table_name)s_%(column_0_name)s",
+#     "ck": "ck_%(table_name)s_%(constraint_name)s",
+#     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+#     "pk": "pk_%(table_name)s"
+# }
+
+# metadata = MetaData(naming_convention=convention)
+
+# db = SQLAlchemy(metadata=metadata)
 
 
-# add any models you may need.
+# class Planet(db.Model, SerializerMixin):
+#     __tablename__ = 'planets_table'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String)
+#     distance_from_earth = db.Column(db.Integer)
+#     nearest_star = db.Column(db.String)
+
+#     # Add relationship
+#     missions = db.relationship("Mission", backref="planet")
+
+#     # Add serialization rules
+
+
+# class Scientist(db.Model, SerializerMixin):
+#     __tablename__ = 'scientists_table'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String, nullable=False)
+#     field_of_study = db.Column(db.String, nullable=False)
+
+#     # Add relationship
+#     missions = db.relationship("Mission", backref="scientist")
+
+#     # Add serialization rules
+
+#     # Add validation
+
+
+# class Mission(db.Model, SerializerMixin):
+#     __tablename__ = 'missions_table'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     name = db.Column(db.String, nullable=False)
+
+#     # Add relationships
+#     scientist_id = db.Column(db.Integer, db.ForeignKey(
+#         'scientists_table.id'), nullable=False)
+#     planet_id = db.Column(db.Integer, db.ForeignKey(
+#         'planets_table.id'), nullable=False)
+
+#     # Add serialization rules
+#     serialize_rules = ('-scientist.missions', '-planet.missions')
+#     # Add validation
+
+
+# # add any models you may need.
